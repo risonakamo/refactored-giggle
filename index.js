4a1d1f641bc5cfec5b6586ac9915aff9e03fcc16
risonakamo <hmykanae@gmail.com>
2018-07-02 02:46:27 -0400

    fix again race condition

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ function main()
     var soundtrigger=document.querySelector(".soundtrigger");
     var videlement=document.querySelector(".sound");
 
-    videlement.addEventListener("play",(e)=>{
-        soundtrigger.parentElement.removeChild(soundtrigger);
-    });
-
     soundtrigger.addEventListener("click",(e)=>{
         videlement.play();
     });
+
+    if (!videlement.paused)
+    {
+        soundtrigger.parentElement.removeChild(soundtrigger);
+    }
+
+    else
+    {
+        videlement.addEventListener("play",(e)=>{
+            soundtrigger.parentElement.removeChild(soundtrigger);
+        });
+    }
 }
